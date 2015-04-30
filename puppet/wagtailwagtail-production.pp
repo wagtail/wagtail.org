00edc5095d90dd24a1105374f997ece3486503e4
Dave Cranwell <david@torchbox.com>
2015-04-30 10:12:36 +0100

    initial commit

---
+++ puppet/wagtailwagtail-production.pp
@@ -0,0 +1,44 @@
+# vim:ts=4 sw=4 et:
+class wagtail::site::production::wagtailwagtailwagtail inherits wagtail::site::production {
+    wagtail::app { 'wagtailwagtailwagtail':
+        ip               => $ipaddress,
+        ip6              => $ipaddress6,
+        manage_ip        => false,
+        manage_db        => true,
+        manage_user      => true,
+        manage_settings  => false,
+        settings         => 'wagtailwagtail/settings',
+        wsgi_module      => 'wagtailwagtail.wsgi',
+        requirements     => 'requirements.txt',
+        servername       => 'wagtailwagtail-production.torchboxapps.com',
+        alias_redirect   => false,
+        codebase_project => '', # CHANGEME
+        codebase_repo    => '', # CHANGEME
+        git_uri          => 'CODEBASE',
+        django_version   => '1.7',
+        staticdir        => "static",
+        mediadir         => "media",
+        deploy           => [ '@admin', '@wagtail' ], # CHANGEME
+        python_version   => '3.4',
+        pg_version       => '9.4',
+        manage_daemons   => [
+            'celery worker -C -c1 -A wagtailwagtail',
+            'celery beat -A wagtailwagtail -C -s $TMPDIR/celerybeat.db --pidfile=',
+        ],
+        admins           => {
+            # CHANGEME
+            # List of users to send error emails to. Eg:
+            # 'Joe Bloggs' => 'joe.bloggs@torchbox.com',
+        },
+        nagios_url       => '/',
+        auth => {
+            enabled       => true,
+            hosts         => [ 'tbx' ],
+            users         => {
+                # CHANGEME
+                # This is the credentials for HTTP authentication. Eg:
+                # 'username'  => 'password',
+            },
+        },
+    }
+}
