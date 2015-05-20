91922909afabaad9515585d97672542997043d39
mope <tom.talbot@torchbox.com>
2015-05-20 16:16:57 +0100

    Migration for last commit

---
+++ wagtailio/core/migrations/0007_auto_20150520_1516.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import django.db.models.deletion
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('core', '0006_featureaspect_title'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='homepagesecondarycarouselitem',
+            name='author_image',
+            field=models.ForeignKey(on_delete=django.db.models.deletion.SET_NULL, to='images.WagtailIOImage', blank=True, related_name='+', null=True),
+            preserve_default=True,
+        ),
+    ]
