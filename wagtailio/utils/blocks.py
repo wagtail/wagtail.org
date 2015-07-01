eab95736b5a5103b53b5731a452410f627868524
Chris Rogers <chris.rogers@torchbox.com>
2015-07-01 14:08:54 +0100

    added codeblock streamfield styles and css

--- wagtailio/utils/blocks.py
+++ wagtailio/utils/blocks.py
@@ -1,6 +1,12 @@
 from django import forms
 
-from wagtail.wagtailcore.blocks import TextBlock, ChooserBlock, StructBlock, ListBlock, StreamBlock, FieldBlock, CharBlock, RichTextBlock, PageChooserBlock, RawHTMLBlock
+from django.utils.safestring import mark_safe
+from markdown import markdown
+from pygments import highlight
+from pygments.formatters import get_formatter_by_name
+from pygments.lexers import get_lexer_by_name
+
+from wagtail.wagtailcore.blocks import TextBlock, ChooserBlock, StructBlock, ListBlock, StreamBlock, FieldBlock, CharBlock, RichTextBlock, PageChooserBlock, RawHTMLBlock, ChoiceBlock
 from wagtail.wagtailimages.blocks import ImageChooserBlock
 from wagtail.wagtailsnippets.blocks import SnippetChooserBlock
 from wagtail.wagtaildocs.blocks import DocumentChooserBlock
@@ -58,6 +64,57 @@ class StatBlock(StructBlock):
     stat = CharBlock()
     text = CharBlock()
 
+### Code and Markdown blocks https://gist.github.com/frankwiles/74a882f16704db9caa27
+
+class CodeBlock(StructBlock):
+    """
+    Code Highlighting Block
+    """
+    LANGUAGE_CHOICES = (
+        ('python', 'Python'),
+        ('bash', 'Bash/Shell'),
+        ('html', 'HTML'),
+        ('css', 'CSS'),
+        ('scss', 'SCSS'),
+    )
+
+    language = ChoiceBlock(choices=LANGUAGE_CHOICES)
+    code = TextBlock()
+
+    class Meta:
+        icon = 'code'
+
+    def render(self, value):
+        src = value['code'].strip('\n')
+        lang = value['language']
+
+        lexer = get_lexer_by_name(lang)
+        formatter = get_formatter_by_name(
+            'html',
+            linenos=None,
+            cssclass='codehilite',
+            style='default',
+            noclasses=False,
+        )
+        return mark_safe(highlight(src, lexer, formatter))
+
+
+class MarkDownBlock(TextBlock):
+    """ MarkDown Block """
+
+    class Meta:
+        icon = 'code'
+
+    def render_basic(self, value):
+        md = markdown(
+            value,
+            [
+                'markdown.extensions.fenced_code',
+                'codehilite',
+            ],
+        )
+        return mark_safe(md)
+
 
 ### Main streamfield block to be inherited by Pages ###
 
@@ -77,3 +134,5 @@ class StoryBlock(StreamBlock):
     tripleimage = TripleImageBlock(icon="image")
     stats = ListBlock(StatBlock(icon="code"))
     embed = EmbedBlock(icon="code")
+    markdown = MarkDownBlock()
+    codeblock = CodeBlock()
