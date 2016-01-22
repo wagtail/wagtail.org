c08aeac9fd79244563c944e84b697cdbe867d69c
Karl Hobley <karlhobley10@gmail.com>
2016-01-22 11:55:10 +0000

    Removed some copy+paste errors
    Didn't notice that...

--- fabfile.py
+++ fabfile.py
@@ -51,38 +51,6 @@ def createsuperuser_staging():
     run('django-admin createsuperuser')
 
 
-@roles('production')
-def deploy_production():
-    # Remove this line when you're happy that this task is correct
-    raise RuntimeError("Please check the fabfile before using it")
-
-    run('git pull origin master')
-    run('pip install -r requirements.txt')
-    run('django-admin migrate --noinput')
-    run('django-admin collectstatic --noinput')
-    run('django-admin compress')
-    run('django-admin update_index')
-
-    # 'restart' should be an alias to a script that restarts the web server
-    run('restart')
-
-
-@roles('staging')
-def deploy_staging():
-    # Remove this line when you're happy that this task is correct
-    raise RuntimeError("Please check the fabfile before using it")
-
-    run('git pull origin staging')
-    run('pip install -r requirements.txt')
-    run('django-admin migrate --noinput')
-    run('django-admin collectstatic --noinput')
-    run('django-admin compress')
-    run('django-admin update_index')
-
-    # 'restart' should be an alias to a script that restarts the web server
-    run('restart')
-
-
 def _pull_data(env_name, remote_db_name, local_db_name, remote_dump_path, local_dump_path):
     timestamp = datetime.now().strftime('%Y%m%d-%I%M%S')
 
