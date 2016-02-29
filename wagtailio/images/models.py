1af42b3e287b1725f6dfa0e2cca06b8079f1fad6
Tom Talbot <tom.talbot@torchbox.com>
2016-02-29 13:46:55 +0000

    Fix image issue caused by Wagtail 1.3 upgrade

--- wagtailio/images/models.py
+++ wagtailio/images/models.py
@@ -7,6 +7,15 @@ from wagtail.wagtailimages.models import AbstractImage, AbstractRendition
 
 class WagtailIOImage(AbstractImage):
     alternative_text = models.CharField(max_length=255, blank=True, help_text="The alternative text for the image, provided for the benefit of visually impaired users")
+    admin_form_fields = (
+        'title',
+        'file',
+        'tags',
+        'focal_point_x',
+        'focal_point_y',
+        'focal_point_width',
+        'focal_point_height',
+    )
 
 
 class WagtailIORendition(AbstractRendition):
