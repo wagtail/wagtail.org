c7a2c701ce24a6242cf668fd78353353cf05f420
Nick Lee <nick@nicklee.co.uk>
2016-08-26 09:35:04 +0100

    Features page

--- wagtailio/static/js/main.js
+++ wagtailio/static/js/main.js
@@ -251,21 +251,4 @@ $(function( ){
         }, function(response){});
     });
 
-    // Facebook code
-    window.fbAsyncInit = function() {
-        FB.init({
-            appId: {{ FB_APP_ID }},
-            xfbml: true,
-            version: 'v2.1'
-        });
-    };
-
-    (function(d, s, id){
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) {return;}
-    js = d.createElement(s); js.id = id;
-    js.src = "https://connect.facebook.net/en_US/sdk.js";
-    fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));
-
 });
