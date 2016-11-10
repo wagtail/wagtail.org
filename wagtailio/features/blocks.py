7768d28bd59b55eeb1d27dd27490391652f22955
Mikalai Radchuk <radchuk.m@gmail.com>
2016-11-10 16:20:17 +0300

    Add additional features block

--- wagtailio/features/blocks.py
+++ wagtailio/features/blocks.py
@@ -14,8 +14,27 @@ class PrimaryFeaturesBlock(blocks.StructBlock):
         template = 'features/blocks/primary_features_block.html'
 
 
+class AdditionalFeaturesBlock(blocks.StructBlock):
+    title = blocks.CharBlock()
+    features_groups = blocks.ListBlock(
+        blocks.StructBlock((
+            ('name', blocks.CharBlock()),
+            ('features', blocks.ListBlock(
+                blocks.StructBlock((
+                    ('title', blocks.CharBlock()),
+                    ('text', blocks.RichTextBlock()),
+                ))
+            )),
+        ))
+    )
+
+    class Meta:
+        template = 'features/blocks/additional_features_block.html'
+
+
 class FeatureIndexPageBlock(blocks.StreamBlock):
     primary_features = PrimaryFeaturesBlock()
+    additional_features = AdditionalFeaturesBlock()
 
     class Meta:
         template = 'features/blocks/feature_index_page_block.html'
