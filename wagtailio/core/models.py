84adf7700aec937e6a0b7a393c99db85e45bb7e9
Mikalai Radchuk <radchuk.m@gmail.com>
2016-12-01 17:55:15 +0300

    Remove HomePageMainCarouselItem and HomePageSecondaryCarouselItem

--- wagtailio/core/models.py
+++ wagtailio/core/models.py
@@ -1,120 +1,22 @@
 from django.db import models
 
 from modelcluster.models import ClusterableModel
-from modelcluster.fields import ParentalKey
 from wagtail.wagtailcore.fields import StreamField
 
 from wagtail.wagtailcore.models import Page, Orderable
-from wagtail.wagtailadmin.edit_handlers import (
-    FieldPanel, MultiFieldPanel,
-    PageChooserPanel,
-    StreamFieldPanel)
-from wagtail.wagtailimages.edit_handlers import ImageChooserPanel
+from wagtail.wagtailadmin.edit_handlers import StreamFieldPanel
 
 from wagtailio.blog.models import BlogPage
 from wagtailio.core.blocks import HomePageBlock
-from wagtailio.utils.models import (
-    SocialMediaMixin,
-    CrossPageMixin,
-)
+from wagtailio.utils.models import SocialMediaMixin, CrossPageMixin
 
-
-# Carousel items
-
-class HomePageMainCarouselItem(Orderable, models.Model):
-    page = ParentalKey('core.HomePage', related_name='main_carousel_items')
-    title = models.CharField(max_length=255)
-    summary = models.CharField(max_length=511)
-    image = models.ForeignKey(
-        'images.WagtailIOImage',
-        null=True,
-        blank=True,
-        on_delete=models.SET_NULL,
-        related_name='+'
-    )
-    video = models.URLField()
-    call_to_action_internal_link = models.ForeignKey(
-        'wagtailcore.Page',
-        null=True,
-        blank=True,
-        related_name='+'
-    )
-    call_to_action_external_link = models.URLField("Call to action URL", blank=True)
-    call_to_action_caption = models.CharField(max_length=255, blank=True)
-
-    @property
-    def call_to_action_link(self):
-        if self.call_to_action_internal_link:
-            return self.call_to_action_internal_link.url
-        else:
-            return self.call_to_action_external_link
-
-    panels = [
-        FieldPanel('title'),
-        FieldPanel('summary'),
-        ImageChooserPanel('image'),
-        FieldPanel('video'),
-        MultiFieldPanel([
-            PageChooserPanel('call_to_action_internal_link'),
-            FieldPanel('call_to_action_external_link'),
-            FieldPanel('call_to_action_caption')
-        ], "Call To Action")
-    ]
-
-
-class HomePageSecondaryCarouselItem(Orderable, models.Model):
-    page = ParentalKey('core.HomePage', related_name='secondary_carousel_items')
-    title = models.CharField(max_length=255)
-    desktop_image = models.ForeignKey(
-        'images.WagtailIOImage',
-        related_name='+'
-    )
-    mobile_image = models.ForeignKey(
-        'images.WagtailIOImage',
-        null=True,
-        blank=True,
-        on_delete=models.SET_NULL,
-        related_name='+'
-    )
-    blockquote = models.TextField()
-    author_name = models.CharField(max_length=255)
-    author_image = models.ForeignKey(
-        'images.WagtailIOImage',
-        null=True,
-        blank=True,
-        on_delete=models.SET_NULL,
-        related_name='+'
-    )
-    author_job = models.CharField(max_length=255)
-    website = models.URLField(blank=True)
-
-    panels = [
-        FieldPanel('title'),
-        ImageChooserPanel('desktop_image'),
-        ImageChooserPanel('mobile_image'),
-        FieldPanel('blockquote'),
-        FieldPanel('author_name'),
-        ImageChooserPanel('author_image'),
-        FieldPanel('author_job'),
-        FieldPanel('website')
-    ]
-
-
-# Homepage
 
 class HomePage(Page, SocialMediaMixin, CrossPageMixin):
-    # TODO: Remove the secondary_carousel_introduction field, when body streamfield is ready
     secondary_carousel_introduction = models.CharField(max_length=511)
     body = StreamField(HomePageBlock())
 
     content_panels = Page.content_panels + [
-        # New fields
         StreamFieldPanel('body'),
-
-        # TODO: Remove the following fields, when body streamfield is ready
-        # InlinePanel('main_carousel_items', label="Main carousel items"),
-        # FieldPanel('secondary_carousel_introduction'),
-        # InlinePanel('secondary_carousel_items', label="Secondary carousel items"),
     ]
 
     promote_panels = Page.promote_panels + SocialMediaMixin.panels + CrossPageMixin.panels
