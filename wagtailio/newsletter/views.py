55f37b5ed7788b0c27cadf58a3978f05f8f1c324
Tomasz Knapik <tomasz.knapik@torchbox.com>
2018-06-05 14:41:31 +0100

    Make newsletter form not use CSRF

--- wagtailio/newsletter/views.py
+++ wagtailio/newsletter/views.py
@@ -1,3 +1,4 @@
+from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
 from django.core.validators import validate_email
 from django.core.exceptions import ValidationError
@@ -6,6 +7,7 @@ from django.shortcuts import render
 from wagtailio.newsletter.models import NewsletterEmailAddress
 
 
+@csrf_exempt
 @require_POST
 def newsletter_signup(request):
     success = False
