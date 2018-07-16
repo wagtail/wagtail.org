6e8bb173cdb8066f2b8684f44045c500692a1bc4
Bertrand Bordage <bordage.bertrand@gmail.com>
2018-07-16 16:44:27 +0200

    Adds backers blocks for crowdfunding campaign blog articles.

--- wagtailio/utils/blocks.py
+++ wagtailio/utils/blocks.py
@@ -6,9 +6,10 @@ from pygments import highlight
 from pygments.formatters import get_formatter_by_name
 from pygments.lexers import get_lexer_by_name
 
-from wagtail.wagtailcore.blocks import TextBlock, ChooserBlock, StructBlock, ListBlock, StreamBlock, FieldBlock, CharBlock, RichTextBlock, PageChooserBlock, RawHTMLBlock, ChoiceBlock
+from wagtail.wagtailcore.blocks import (
+    TextBlock, StructBlock, ListBlock, StreamBlock, FieldBlock, CharBlock,
+    RichTextBlock, ChoiceBlock, URLBlock)
 from wagtail.wagtailimages.blocks import ImageChooserBlock
-from wagtail.wagtailsnippets.blocks import SnippetChooserBlock
 from wagtail.wagtaildocs.blocks import DocumentChooserBlock
 from wagtail.wagtailembeds.blocks import EmbedBlock
 
@@ -118,6 +119,40 @@ class MarkDownBlock(TextBlock):
         return mark_safe(md)
 
 
+class NamedBackerBlock(StructBlock):
+    name = CharBlock()
+
+    class Meta:
+        template = 'blog/blocks/named_backer.html'
+
+
+class LinkedBackerBlock(NamedBackerBlock):
+    url = URLBlock(required=False)
+
+    class Meta:
+        template = 'blog/blocks/linked_backer.html'
+
+
+class ImageBackerBlock(StructBlock):
+    name = CharBlock()
+    image = ImageChooserBlock(required=False)
+    url = URLBlock(required=False)
+
+    class Meta:
+        template = 'blog/blocks/image_backer.html'
+
+
+class BackersBlock(StructBlock):
+    gold_backers = ListBlock(ImageBackerBlock())
+    silver_backers = ListBlock(ImageBackerBlock())
+    bronze_backers = ListBlock(ImageBackerBlock())
+    linked_backers = ListBlock(LinkedBackerBlock())
+    named_backers = ListBlock(NamedBackerBlock())
+
+    class Meta:
+        template = 'blog/blocks/backers.html'
+
+
 ### Main streamfield block to be inherited by Pages ###
 
 class StoryBlock(StreamBlock):
@@ -138,3 +173,4 @@ class StoryBlock(StreamBlock):
     embed = EmbedBlock(icon="code")
     markdown = MarkDownBlock()
     codeblock = CodeBlock()
+    backers = BackersBlock()
