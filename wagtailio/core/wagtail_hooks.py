56f1ac008500f49913a7c000a1195115291ea91a
Tomasz Knapik <tomasz.knapik@torchbox.com>
2018-08-29 00:15:52 +0100

    Serve files directly from S3 without signing

--- wagtailio/core/wagtail_hooks.py
+++ wagtailio/core/wagtail_hooks.py
@@ -3,14 +3,12 @@ from django.core.files.storage import get_storage_class
 from django.shortcuts import redirect
 from django.utils.cache import add_never_cache_headers
 from django.utils.html import format_html, format_html_join
-from django.utils.safestring import mark_safe
 from wagtail.core import hooks
 from wagtail.core.whitelist import allow_without_attributes
 from wagtail.documents.models import document_served, get_document_model
 
 from storages.backends.s3boto3 import S3Boto3Storage
 
-from wagtailio.storages.backends import S3Boto3StorageWithQuerystring
 
 
 def editor_js():
@@ -67,8 +65,8 @@ def serve_document_from_s3(document, request):
     document_served.send(sender=get_document_model(), instance=document,
                          request=request)
 
-    # Generate signed URL so it can have private ACL set on S3.
-    file_url = S3Boto3StorageWithQuerystring().url(document.file.name)
+    # Service file directly from S3.
+    file_url = document.file.url
 
     # Generate redirect response and add never_cache headers.
     # Delete all existing headers.
