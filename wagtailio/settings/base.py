52683e7ecba341d1bee9c6258247f90d026561b2
Dan Braghis <dan.braghis@torchbox.com>
2020-07-07 17:49:39 +0100

    Upgrade Wagtail and Pillow

--- wagtailio/settings/base.py
+++ wagtailio/settings/base.py
@@ -92,7 +92,6 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
-    "wagtail.core.middleware.SiteMiddleware",
     "wagtail.contrib.redirects.middleware.RedirectMiddleware",
 ]
 
