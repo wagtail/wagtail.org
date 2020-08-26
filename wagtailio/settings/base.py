f8d1cde9f6d870d19f08c21d73ccc414e9ba2abf
Tom Dyson <tom@torchbox.com>
2020-08-26 09:02:48 +0100

    Make embeds responsive again

--- wagtailio/settings/base.py
+++ wagtailio/settings/base.py
@@ -379,6 +379,9 @@ WAGTAILIMAGES_IMAGE_MODEL = "images.WagtailioImage"
 
 if "PRIMARY_HOST" in env:
     BASE_URL = "http://%s/" % env["PRIMARY_HOST"]
+    
+# https://docs.wagtail.io/en/v2.8.1/releases/2.8.html#responsive-html-for-embeds-no-longer-added-by-default
+WAGTAILEMBEDS_RESPONSIVE_HTML = True
 
 
 # Search
