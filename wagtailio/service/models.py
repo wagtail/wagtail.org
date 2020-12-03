379e92f13cf293fea378538c3cb51107dd24cef7
Brian Xu <brian.xu@torchbox.com>
2020-12-03 19:47:53 +0800

    ServicePage inherited Page, ContentImportMixin, SocialMediaMixin, CrossPageMixin

--- wagtailio/service/models.py
+++ wagtailio/service/models.py
@@ -1,5 +1,11 @@
-from wagtailio.standardpage.models import StandardPage
+from wagtail.core.models import Page
+from wagtail_content_import.models import ContentImportMixin
+from wagtailio.utils.models import SocialMediaMixin, CrossPageMixin
 
 
-class ServicePage(StandardPage):
+class ServicePage(Page, ContentImportMixin, SocialMediaMixin, CrossPageMixin):
     template = "service/services_page.html"
+
+    promote_panels = (
+        Page.promote_panels + SocialMediaMixin.panels + CrossPageMixin.panels
+    )
