7463ed1a2b24ef9a733567d2f687888fd2e9d1f2
Tibor Leupold <tibor.leupold@torchbox.com>
2021-03-11 16:48:36 -1000

    Add intro field

--- wagtailio/services/models.py
+++ wagtailio/services/models.py
@@ -1,5 +1,11 @@
+from django.db import models
+from wagtail.admin import edit_handlers
 from wagtail.core.models import Page
 
 
 class ServicesPage(Page):
-    pass
+    intro = models.TextField(null=True, blank=True)
+
+    content_panels = Page.content_panels + [
+        edit_handlers.FieldPanel("intro"),
+    ]
