20165f8951a352355e040e05e12da78968cc2861
Tibor Leupold <tibor.leupold@torchbox.com>
2021-03-11 17:50:38 -1000

    Add section content stream block

--- wagtailio/services/blocks.py
+++ wagtailio/services/blocks.py
@@ -2,8 +2,17 @@ from wagtail.core import blocks
 from wagtail.core import fields
 
 
+class SectionContentBlock(blocks.StreamBlock):
+    heading = blocks.CharBlock(max_length=250, form_classname="full")
+
 class SectionBlock(blocks.StructBlock):
-    title = blocks.CharBlock(required=True)
+    title = blocks.CharBlock(required=True,  form_classname="full title")
+    # TODO: Section icon
+    # TODO: Section image (optional)
+    # image = ImageAndCaptionBlock(required=False)
+    # TODO: Section background color
+
+    content = SectionContentBlock(required=False, form_classname="full")
 
     class Meta:
         icon = "cogs"
