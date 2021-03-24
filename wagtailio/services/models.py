b86a95ea56350f80764009fc1ae8c3a20fd68f85
Tibor Leupold <tibor.leupold@torchbox.com>
2021-03-23 17:02:59 -1000

    Fix num section blocks to 4

--- wagtailio/services/models.py
+++ wagtailio/services/models.py
@@ -1,15 +1,22 @@
 from django.db import models
 from wagtail.admin import edit_handlers
-from wagtail.core import fields
+from wagtail.core import blocks, fields
 from wagtail.core.models import Page
 
 from wagtailio.services.blocks import SectionBlock
 
+
 class ServicesPage(Page):
     intro = models.TextField(null=True, blank=True)
-    body = fields.StreamField([
-        ('section', SectionBlock())
-    ], null=True, blank=True)
+    body = fields.StreamField(
+        blocks.StreamBlock(
+            [('section', SectionBlock())],
+            min_num=4,
+            max_num=4,
+        ),
+        null=True,
+        blank=False,
+    )
 
     content_panels = Page.content_panels + [
         edit_handlers.FieldPanel("intro"),
