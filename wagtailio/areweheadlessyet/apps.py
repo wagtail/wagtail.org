00fa2cdf852a14ee4db90238ed96189850ee4563
Tijani-Dia <71412737+Tijani-Dia@users.noreply.github.com>
2022-01-19 13:39:05 +0100

    Update app config name to follow Django's conventions
    Co-authored-by: Thibaud Colas <thibaudcolas@gmail.com>

--- wagtailio/areweheadlessyet/apps.py
+++ wagtailio/areweheadlessyet/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class AreweheadlessyetConfig(AppConfig):
+class AreWeHeadlessYetConfig(AppConfig):
     name = "wagtailio.areweheadlessyet"
     default_auto_field = "django.db.models.BigAutoField"
