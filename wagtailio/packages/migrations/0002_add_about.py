0da8b0f348b8f48242464746df92ec4a2adbd81d
Jake Howard <jake.howard@torchbox.com>
2022-01-28 09:49:41 +0000

    Don't edit existing migration
    The default had changed, but because we edited the migration, Django never picked up on it

--- wagtailio/packages/migrations/0002_add_about.py
+++ wagtailio/packages/migrations/0002_add_about.py
@@ -14,7 +14,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='packagespage',
             name='about_text',
-            field=wagtail.core.fields.RichTextField(default='<p> Projects listed on Wagtail.org are <i>third-party</i> packages.<br/> They are not vetted nor endorsed by Wagtail.<br/> Use them at your own risk.</p> <p>This page collects girds and packages from djangopackages.org.<br/> Please add or update Wagtail grids and Wagtail packages on djangopackages.org. </p>'),
+            field=wagtail.core.fields.RichTextField(default='<p> Projects listed on Wagtail.io are <i>third-party</i> packages.<br/> They are not vetted nor endorsed by Wagtail.<br/> Use them at your own risk.</p> <p>This page collects girds and packages from djangopackages.org.<br/> Please add or update Wagtail grids and Wagtail packages on djangopackages.org. </p>'),
         ),
         migrations.AddField(
             model_name='packagespage',
