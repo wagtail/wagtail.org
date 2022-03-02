5ceb5c44b24c7f1dea40b6e6add02479dee9ab7b
Dan Braghis <dan.braghis@torchbox.com>
2022-03-02 21:04:33 +0000

    Add sitewide alert app

---
+++ wagtailio/sitewide_alert/views.py
@@ -0,0 +1,26 @@
+from django.conf import settings
+from django.http import JsonResponse
+from django.utils.cache import patch_cache_control
+
+from wagtail.core.models import Site
+from wagtail.core.rich_text import expand_db_html
+
+from .models import SiteWideAlertSettings
+
+
+def sitewide_alert(request):
+    site = Site.find_for_request(request)
+    alert_settings = SiteWideAlertSettings.for_site(site)
+    data = {}
+    if alert_settings.sitewide_alert_enabled:
+        data = {
+            "text": expand_db_html(alert_settings.sitewide_alert_text),
+        }
+    response = JsonResponse(data)
+
+    # Browser should cache only briefly
+    max_age = getattr(settings, "SITEWIDE_ALERT_MAXAGE", 300)
+    # FE cache can cache indefinitely, as will be purged on change
+    s_maxage = getattr(settings, "SITEWIDE_ALERT_SMAXAGE", 604800)
+    patch_cache_control(response, max_age=max_age, s_maxage=s_maxage, public=True)
+    return response
