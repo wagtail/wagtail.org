2380b0a31f48a7b6fa5a9f07bb13a00a65f275c0
Tidiane Dia <atdia97@gmail.com>
2022-03-03 17:07:21 +0000

    Modify the API to serve content from AreWeHeadlessYet site

--- wagtailio/api.py
+++ wagtailio/api.py
@@ -1,8 +1,24 @@
 from wagtail.api.v2.router import WagtailAPIRouter
 from wagtail.api.v2.views import PagesAPIViewSet
-from wagtail.images.api.v2.views import ImagesAPIViewSet
+from wagtail.core.models import Page
+
+from wagtailio.areweheadlessyet.models import AreWeHeadlessYetHomePage
 
 api_router = WagtailAPIRouter("wagtailapi")
 
-api_router.register_endpoint("pages", PagesAPIViewSet)
-api_router.register_endpoint("images", ImagesAPIViewSet)
+
+class AreWeHeadlessYetPagesAPIViewSet(PagesAPIViewSet):
+    def get_base_queryset(self):
+        """Returns a queryset containing only pages from the AreWeHeadLessYet site."""
+
+        areweheadlessyet_queryset = AreWeHeadlessYetHomePage.objects.all()
+        if not areweheadlessyet_queryset:
+            return areweheadlessyet_queryset
+
+        areweheadlessyet_root_page = areweheadlessyet_queryset[0]
+        return Page.objects.live().descendant_of(
+            areweheadlessyet_root_page, inclusive=True
+        )
+
+
+api_router.register_endpoint("pages", AreWeHeadlessYetPagesAPIViewSet)
