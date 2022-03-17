65c4d89211457117f1088fbf855db6a338388796
Tidiane Dia <atdia97@gmail.com>
2022-03-17 17:41:24 +0000

    Simplify AreWeHeadlessYet home page querying

--- wagtailio/api.py
+++ wagtailio/api.py
@@ -11,14 +11,13 @@ class AreWeHeadlessYetPagesAPIViewSet(PagesAPIViewSet):
     def get_base_queryset(self):
         """Returns a queryset containing only pages from the AreWeHeadLessYet site."""
 
-        areweheadlessyet_queryset = AreWeHeadlessYetHomePage.objects.all()
-        if not areweheadlessyet_queryset:
-            return areweheadlessyet_queryset
-
-        areweheadlessyet_root_page = areweheadlessyet_queryset[0]
-        return Page.objects.live().descendant_of(
-            areweheadlessyet_root_page, inclusive=True
-        )
+        areweheadlessyet_root_page = AreWeHeadlessYetHomePage.objects.first()
+        if areweheadlessyet_root_page:
+            return Page.objects.live().descendant_of(
+                areweheadlessyet_root_page, inclusive=True
+            )
+        else:
+            return Page.objects.none()
 
 
 api_router.register_endpoint("pages", AreWeHeadlessYetPagesAPIViewSet)
