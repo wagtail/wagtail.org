1cd5e027e50baa1e57d4d1b8136b72a188b0de50
Tidiane Dia <atdia97@gmail.com>
2022-05-11 09:13:51 +0100

    Move deploy signal handler outside function to avoid it being garbage collected

--- wagtailio/areweheadlessyet/signal_handlers.py
+++ wagtailio/areweheadlessyet/signal_handlers.py
@@ -6,20 +6,10 @@ from .models import AreWeHeadlessYetHomePage, AreWeHeadlessYetTopicPage
 
 
 def register_signal_handlers():
-    deploy_url = getattr(settings, "VERCEL_DEPLOY_HOOK_URL", None)
-    if not deploy_url:
+    if not getattr(settings, "VERCEL_DEPLOY_HOOK_URL", None):
         return
 
-    from .thread_pool import run_in_thread_pool
-
-    @run_in_thread_pool
-    def deploy(sender, **kwargs):
-        """Triggers a build on Vercel."""
-
-        try:
-            requests.post(deploy_url, timeout=settings.VERCEL_DEPLOY_REQUEST_TIMEOUT)
-        except requests.exceptions.Timeout:
-            pass  # Ignore this error
+    from .thread_pool import deploy
 
     page_published.connect(deploy, sender=AreWeHeadlessYetHomePage)
     page_published.connect(deploy, sender=AreWeHeadlessYetTopicPage)
