1852aa6e26af68a0c4e1307fe89449662ec47d85
Chris Lawton <chris.lawton@torchbox.com>
2022-07-20 09:49:09 +0100

    remove compressor

--- wagtailio/settings/production.py
+++ wagtailio/settings/production.py
@@ -1,25 +1,6 @@
 from .base import *  # noqa
 from .base import env
 
-
-# Compress static files offline and minify CSS
-# http://django-compressor.readthedocs.org/en/latest/settings/#django.conf.settings.COMPRESS_OFFLINE
-COMPRESS_OFFLINE = True
-
-# Use local cache for Django compressor so we can build it in Docker
-CACHES["compressor_cache"] = {  # noqa
-    "BACKEND": "django.core.cache.backends.locmem.LocMemCache"
-}
-COMPRESS_CACHE_BACKEND = "compressor_cache"
-
-COMPRESS_FILTERS = {
-    "css": [
-        "compressor.filters.css_default.CssAbsoluteFilter",
-        "compressor.filters.cssmin.CSSMinFilter",
-    ],
-    'js': ['compressor.filters.jsmin.JSMinFilter']
-}
-
 COMPUTER_VISION_API_KEY = env.get('COMPUTER_VISION_API_KEY')
 COMPUTER_VISION_REGION = env.get('COMPUTER_VISION_REGION')
 ALT_GENERATOR_MAX_TAGS = env.get("ALT_GENERATOR_MAX_TAGS", 5)
