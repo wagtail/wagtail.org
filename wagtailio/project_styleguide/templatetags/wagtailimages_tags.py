a73c7c80b8c6f270d1279542f174abbc2f838406
Chris Lawton <chris.lawton@torchbox.com>
2022-08-01 15:56:01 +0100

    lint isort

--- wagtailio/project_styleguide/templatetags/wagtailimages_tags.py
+++ wagtailio/project_styleguide/templatetags/wagtailimages_tags.py
@@ -1,4 +1,5 @@
-from pattern_library.monkey_utils import override_tag
 from wagtail.images.templatetags.wagtailimages_tags import register
+
+from pattern_library.monkey_utils import override_tag
 
 override_tag(register, name="image")
