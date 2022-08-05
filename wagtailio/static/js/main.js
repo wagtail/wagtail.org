fdf45f7ac5b4165a576b84319e178f1abf2c082c
Chris Lawton <chris.lawton@torchbox.com>
2022-08-05 13:47:05 +0100

    add the get started menu modal

--- wagtailio/static/js/main.js
+++ wagtailio/static/js/main.js
@@ -1,1 +1,12 @@
 import '../sass/main.scss';
+
+import GetStartedMenu from './components/get-started-menu';
+
+function initComponent(ComponentClass) {
+    const items = document.querySelectorAll(ComponentClass.selector());
+    items.forEach((item) => new ComponentClass(item));
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    initComponent(GetStartedMenu);
+});
