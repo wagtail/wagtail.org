5b291a7520f13498d7630e5b5d472ff1c94abc40
Steven Steinwand <steven_ts@hotmail.com>
2022-08-11 11:13:38 -0600

    Adjust search page as per design feedback

--- wagtailio/project_styleguide/templatetags/wagtailcore_tags.py
+++ wagtailio/project_styleguide/templatetags/wagtailcore_tags.py
@@ -1,7 +1,10 @@
 from wagtail.core.templatetags.wagtailcore_tags import register
+from django.template.defaulttags import register as django_register
 
 from pattern_library.monkey_utils import override_tag
 
 override_tag(register, name="include_block")
 override_tag(register, name="pageurl")
 override_tag(register, name="slugurl")
+
+override_tag(django_register, name="url")
