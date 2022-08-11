f283d0ae46dfef318120e11fcd6a4af711465bdc
Victor Miti <victor.miti@torchbox.com>
2022-08-12 01:11:17 +0200

    Add taxonomy app

---
+++ wagtailio/taxonomy/wagtail_hooks.py
@@ -0,0 +1,21 @@
+from wagtail.contrib.modeladmin.options import (
+    ModelAdmin,
+    ModelAdminGroup,
+    modeladmin_register,
+)
+
+from wagtailio.taxonomy.models import Category
+
+
+class CategoryModelAdmin(ModelAdmin):
+    model = Category
+    menu_icon = "tag"
+
+
+class TaxonomiesModelAdminGroup(ModelAdminGroup):
+    menu_label = "Taxonomies"
+    items = (CategoryModelAdmin,)
+    menu_icon = "folder-inverse"
+
+
+modeladmin_register(TaxonomiesModelAdminGroup)
