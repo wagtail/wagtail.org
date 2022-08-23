13f32af68e7dc24d8cc48fd0819befff3ad7ce7b
Chris Lawton <chris.lawton@torchbox.com>
2022-08-23 13:32:03 +0100

    move 404 and 500 templates into pattern library

--- wagtailio/urls.py
+++ wagtailio/urls.py
@@ -58,11 +58,11 @@ if settings.DEBUG:
         # Add views for testing 404 and 500 templates
         path(
             "test404/",
-            TemplateView.as_view(template_name="patterns/404.html"),
+            TemplateView.as_view(template_name="patterns/pages/errors/404.html"),
         ),
         path(
             "test500/",
-            TemplateView.as_view(template_name="patterns/500.html"),
+            TemplateView.as_view(template_name="patterns/pages/errors/500.html"),
         ),
     ]
 
