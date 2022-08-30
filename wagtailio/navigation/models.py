9d1181a4e0d6ccf42e74da9718afa8117f24c68e
Victor Miti <victor.miti@torchbox.com>
2022-08-30 21:04:31 +0200

    Add Snippets for Main Menu Item and Main Menu
    and also update NavigationSettings to include Get Started Menu

--- wagtailio/navigation/models.py
+++ wagtailio/navigation/models.py
@@ -1,13 +1,21 @@
 from django.db import models
 
+from modelcluster.fields import ParentalKey
 from modelcluster.models import ClusterableModel
-from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel
+from wagtail.admin.edit_handlers import (
+    FieldPanel,
+    InlinePanel,
+    MultiFieldPanel,
+    StreamFieldPanel,
+)
 from wagtail.contrib.settings.models import BaseSetting, register_setting
 from wagtail.core.fields import StreamField
+from wagtail.core.models import Orderable
 from wagtail.snippets.edit_handlers import SnippetChooserPanel
 from wagtail.snippets.models import register_snippet
 
-from wagtailio.navigation.blocks import NavStreamField
+from wagtailio.navigation.blocks import NavItemBlock, NavItemCTA, NavStreamField
+from wagtailio.navigation.forms import MainMenuSnippetForm
 
 
 @register_snippet
@@ -29,8 +37,80 @@ class FooterMenuSnippet(models.Model):
         verbose_name = "Footer menu"
 
 
+@register_snippet
+class MainMenuItemSnippet(models.Model):
+    name = models.CharField(max_length=255)
+    nav_items = StreamField(
+        [("nav_item", NavItemBlock())],
+    )
+    call_to_action = StreamField(
+        [("cta", NavItemCTA())],
+        blank=True,
+        max_num=1,
+    )
+
+    panels = [
+        FieldPanel("name"),
+        StreamFieldPanel("nav_items", classname="collapsible"),
+        StreamFieldPanel("call_to_action", classname="collapsible"),
+    ]
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Main menu item"
+
+
+class MainMenuItem(Orderable):
+    parent = ParentalKey("navigation.MainMenuSnippet", related_name="menu_items")
+    menu_item = models.ForeignKey(
+        "navigation.MainMenuItemSnippet",
+        on_delete=models.CASCADE,
+    )
+
+    panels = [
+        SnippetChooserPanel("menu_item"),
+    ]
+
+
+@register_snippet
+class MainMenuSnippet(ClusterableModel):
+    base_form_class = MainMenuSnippetForm
+
+    name = models.CharField(max_length=255)
+
+    panels = [
+        FieldPanel("name"),
+        MultiFieldPanel(
+            [InlinePanel("menu_items", label="Menu item", min_num=1, max_num=10)],
+            heading="Menu items",
+        ),
+    ]
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Main menu"
+
+
 @register_setting(icon="list-ul")
 class NavigationSettings(BaseSetting, ClusterableModel):
+    get_started_menu = models.ForeignKey(
+        "core.GetStartedSnippet",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="+",
+    )
+    main_navigation = models.ForeignKey(
+        "navigation.MainMenuSnippet",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="+",
+    )
     footer_navigation = models.ForeignKey(
         "navigation.FooterMenuSnippet",
         null=True,
@@ -40,5 +120,7 @@ class NavigationSettings(BaseSetting, ClusterableModel):
     )
 
     panels = [
+        SnippetChooserPanel("get_started_menu"),
+        SnippetChooserPanel("main_navigation"),
         SnippetChooserPanel("footer_navigation"),
     ]
