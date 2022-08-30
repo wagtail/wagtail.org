0211199dc667ec365809334d33db9c65f2b5c15d
Victor Miti <victor.miti@torchbox.com>
2022-08-30 21:08:20 +0200

    Update navigatin_tags and templates
    templates:
    - patterns/includes/header.html
    - patterns/includes/menu_get_started.html
    - patterns/includes/menu_main.html

--- wagtailio/navigation/templatetags/navigation_tags.py
+++ wagtailio/navigation/templatetags/navigation_tags.py
@@ -5,10 +5,49 @@ register = template.Library()
 
 @register.inclusion_tag("patterns/includes/menu_footer.html", takes_context=True)
 def footer_navigation(context):
-    request = context["request"]
-    return {
-        "footer_navigation": context["settings"]["navigation"][
+    try:
+        footer_navigation = context["settings"]["navigation"][
             "NavigationSettings"
-        ].footer_navigation,
-        "request": request,
+        ].footer_navigation
+    except (KeyError, AttributeError):
+        footer_navigation = None
+
+    return {
+        "footer_navigation": footer_navigation,
+    }
+
+
+@register.inclusion_tag("patterns/includes/menu_main.html", takes_context=True)
+def main_navigation(context):
+    try:
+        main_navigation = context["settings"]["navigation"][
+            "NavigationSettings"
+        ].main_navigation
+    except (KeyError, AttributeError):
+        return {}
+
+    menus = []
+    for item in main_navigation.menu_items.all():
+        menus.append(
+            {
+                "name": item.menu_item.name,
+                "nav_items": item.menu_item.nav_items,
+                "call_to_action": item.menu_item.call_to_action,
+            }
+        )
+
+    return {"menus": menus}
+
+
+@register.inclusion_tag("patterns/includes/menu_get_started.html", takes_context=True)
+def get_started_menu(context):
+    try:
+        get_started_menu = context["settings"]["navigation"][
+            "NavigationSettings"
+        ].get_started_menu
+    except (KeyError, AttributeError):
+        get_started_menu = None
+
+    return {
+        "get_started_menu": get_started_menu,
     }
