ab216320025678deed0a4c92b2ea393788bcecf5
Tom Usher <tom@tomusher.com>
2022-11-10 10:06:05 +0000

    Set AUTH_PASSWORD_VALIDATORS to enable common password validation rules

--- wagtailio/settings/base.py
+++ wagtailio/settings/base.py
@@ -104,6 +104,18 @@ MIDDLEWARE = [
 ROOT_URLCONF = "wagtailio.urls"
 WSGI_APPLICATION = "wagtailio.wsgi.application"
 
+# Password validation
+# https://docs.djangoproject.com/en/stable/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"
+    },
+    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator"},
+    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator"},
+    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
+]
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
