03f682a63f673c43a33735ba57d927b7585053bb
Sage Abdullah <sage.abdullah@torchbox.com>
2023-04-26 08:14:18 +0700

    Remove dangling milestone items and any deleted milestones

--- wagtailio/roadmap/views.py
+++ wagtailio/roadmap/views.py
@@ -65,6 +65,8 @@ def process(import_all=True):
     )
     data = response.json()["data"]
     milestone_nodes = data["repository"]["milestones"]["nodes"]
+    seen_milestone_numbers = set()
+    seen_item_numbers = set()
 
     for node in milestone_nodes:
         due_on = parse_datetime(node["dueOn"]).date() if node["dueOn"] else None
@@ -78,12 +80,14 @@ def process(import_all=True):
                 "url": node["url"],
             },
         )
+        seen_milestone_numbers.add(node["number"])
 
         for issue in node["issues"]["nodes"]:
             labels = {label["name"] for label in issue["labels"]["nodes"]}
             item = Item.objects.filter(number=issue["number"]).first()
             if not item:
                 item = Item(number=issue["number"])
+            seen_item_numbers.add(issue["number"])
 
             item.state = issue["state"]
             item.title = issue["title"]
@@ -92,6 +96,17 @@ def process(import_all=True):
             item.labels = ",".join(labels)
             item.full_clean()
             item.save()
+
+    # If importing all milestones, remove milestones that are not in the response.
+    if import_all:
+        Milestone.objects.exclude(number__in=seen_milestone_numbers).delete()
+
+    # Remove items that are no longer attached to the seen milestones.
+    (
+        Item.objects.filter(milestone__number__in=seen_milestone_numbers)
+        .exclude(number__in=seen_item_numbers)
+        .delete()
+    )
 
 
 class ImportView(TemplateView):
