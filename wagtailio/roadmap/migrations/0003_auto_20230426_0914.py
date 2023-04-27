4143dbbd061eb72788bb41802a5fde704bda42aa
Sage Abdullah <sage.abdullah@torchbox.com>
2023-04-27 17:05:12 +0700

    Format code

--- wagtailio/roadmap/migrations/0003_auto_20230426_0914.py
+++ wagtailio/roadmap/migrations/0003_auto_20230426_0914.py
@@ -4,7 +4,6 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
         ("roadmap", "0002_roadmappage"),
     ]
--- wagtailio/roadmap/migrations/0004_auto_20230426_1220.py
+++ wagtailio/roadmap/migrations/0004_auto_20230426_1220.py
@@ -4,7 +4,6 @@ from django.db import migrations
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
         ("roadmap", "0003_auto_20230426_0914"),
     ]
