fb0d729e3e5374ebd9391667a8dd8f571ce38e8b
Sage Abdullah <sage.abdullah@torchbox.com>
2023-05-02 13:24:38 +0700

    Allow supplying GitHub access token during import

---
+++ wagtailio/roadmap/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from django.conf import settings
+
+requires_token = not settings.GITHUB_ROADMAP_ACCESS_TOKEN
+
+
+class ImportForm(forms.Form):
+    github_token = forms.CharField(
+        max_length=64,
+        required=requires_token,
+        label="GitHub access token",
+        help_text="Optional — if not supplied, a default token will be used"
+        if not requires_token
+        else None,
+    )
