abacbd00a771ae6d6767563fe81e6d70aec63e51
William Blackie <>
2023-12-13 15:21:44 +0000

    Create showcasePage and related models

---
+++ wagtailio/core/migrations/0061_create_initial_sectors.py
@@ -0,0 +1,30 @@
+from django.db import migrations
+
+SECTOR_DATA = (
+    "Government agencies",
+    "Corporations",
+    "Education",
+    "Cultural institutions",
+    "Charities and nonprofits"
+)
+
+
+def _populate_sector_data(apps, schema_editor):
+    Sector = apps.get_model("core", "Sector")
+    to_create = []
+    for name in SECTOR_DATA:
+        to_create.append(Sector(name=name))
+    Sector.objects.bulk_create(to_create)
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("core", "0060_sector_showcaseitem_showcasepage"),
+    ]
+
+    operations = [
+        migrations.RunPython(
+            code=_populate_sector_data,
+            reverse_code=migrations.RunPython.noop,
+        ),
+    ]
--- wagtailio/core/models/pages.py
+++ wagtailio/core/models/pages.py
@@ -1,8 +1,10 @@
+from django.core.exceptions import ValidationError
 from django.db import models
 
-from wagtail.admin.panels import FieldPanel, MultiFieldPanel
+from modelcluster.fields import ParentalKey
+from wagtail.admin.panels import FieldPanel, InlinePanel, MultiFieldPanel
 from wagtail.fields import RichTextField, StreamField
-from wagtail.models import Page
+from wagtail.models import Orderable, Page
 from wagtail.search import index
 
 from wagtailmedia.edit_handlers import MediaChooserPanel
@@ -19,6 +21,7 @@ class HomePage(SocialMediaMixin, CrossPageMixin, Page):
     subpage_types = [
         "blog.BlogIndexPage",
         "core.ContentPage",
+        "core.ShowcasePage",
         "features.FeatureIndexPage",
         "newsletter.NewsletterIndexPage",
         "standardpage.StandardPage",
@@ -130,3 +133,139 @@ class ContentPage(Page, HeroMixin, SocialMediaMixin, CrossPageMixin):
         index.SearchField("intro"),
         index.SearchField("body"),
     ]
+
+
+class ShowcasePage(Page, SocialMediaMixin, CrossPageMixin):
+    template = "patterns/pages/showcase_page/showcase_page.html"
+    htmx_template = "patterns/pages/showcase_page/showcase_page_fragment.html"
+
+    parent_page_types = ["core.HomePage"]
+
+    cta = StreamField(
+        [("cta", CTABlock())],
+        blank=True,
+        max_num=1,
+        help_text="Allows for a maximum of 1 CTA blocks",
+        use_json_field=True,
+    )
+
+    content_panels = Page.content_panels + [
+        FieldPanel("cta"),
+        InlinePanel("showcase_items", label="Showcase items"),
+    ]
+
+    promote_panels = (
+        Page.promote_panels + SocialMediaMixin.panels + CrossPageMixin.panels
+    )
+
+    search_fields = Page.search_fields + [
+        index.SearchField("heading"),
+        index.SearchField("sub_heading"),
+        index.SearchField("intro"),
+        index.SearchField("body"),
+    ]
+
+    def _is_htmx_request(self, request):
+        return request.headers.get("HX-Request") == "true"
+
+    def _filter_used_sectors(self):
+        return ["All"] + list(
+            self.showcase_items.values_list("sector__name", flat=True).distinct()
+        )
+
+    def _filtered_showcase_items(self, sector):
+        if sector in self._filter_used_sectors() and sector != "All":
+            return self.showcase_items.filter(sector__name=sector)
+        else:
+            return self.showcase_items.all()
+
+    def get_template(self, request, *args, **kwargs):
+        if self._is_htmx_request(request):
+            return self.htmx_template
+        else:
+            return self.template
+
+    def serve(self, request):
+        response = super().serve(request)
+        if self._is_htmx_request(request):
+            # We only return the fragment, rather than the whole page.
+            new_url = self.url + "?" + request.GET.urlencode()
+            response.headers["HX-Push"] = new_url
+        return response
+
+    def get_context(self, request):
+        context = super().get_context(request)
+
+        context |= {
+            "sectors": self._filter_used_sectors(),
+            "showcase_items": self._filtered_showcase_items(request.GET.get("sector")),
+            "current_sector": request.GET.get("sector") or "All",
+        }
+
+        return context
+
+
+class ShowcaseItem(Orderable):
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    image = models.ForeignKey(
+        "images.WagtailIOImage",
+        models.SET_NULL,
+        null=True,
+        related_name="+",
+    )
+    logo = models.ForeignKey(
+        "images.WagtailIOImage",
+        models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="+",
+    )
+    external_link = models.URLField(blank=True)
+    internal_link = models.ForeignKey(
+        "wagtailcore.Page",
+        models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="+",
+    )
+
+    sector = models.ForeignKey(
+        "Sector",
+        models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="+",
+    )
+
+    page = ParentalKey(
+        ShowcasePage,
+        related_name="showcase_items",
+        on_delete=models.CASCADE,
+    )
+
+    panels = [
+        FieldPanel("title"),
+        FieldPanel("sector"),
+        FieldPanel("description"),
+        FieldPanel("image"),
+        FieldPanel("logo"),
+        FieldPanel("external_link"),
+        FieldPanel("internal_link"),
+    ]
+
+    def clean(self):
+        if not self.external_link and not self.internal_link:
+            raise ValidationError(
+                "You must specify either an external or internal link"
+            )
+        if self.external_link and self.internal_link:
+            raise ValidationError(
+                "You must specify either an external or internal link, not both"
+            )
+
+        if self.logo and not self.image:
+            raise ValidationError("You must specify an image when using a logo")
+
+    def __str__(self):
+        return self.title
