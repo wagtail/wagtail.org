7fe4dcc50b702a1524431ba52992e26a8a9fe07e
William Blackie <>
2024-01-02 16:04:58 +0000

    Refactor models to new app

--- wagtailio/core/models/snippets.py
+++ wagtailio/core/models/snippets.py
@@ -72,15 +72,3 @@ class SignupFormSnippet(models.Model):
 
     class Meta:
         verbose_name = "Signup form"
-
-
-@register_snippet
-class Sector(models.Model):
-    name = models.CharField(max_length=255, unique=True)
-
-    panels = [
-        FieldPanel("name"),
-    ]
-
-    def __str__(self):
-        return self.name
