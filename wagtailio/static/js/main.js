f1cfedc8f0c5857c05c92440cb48efcd84dad4e8
Thibaud Colas <thibaudcolas@gmail.com>
2024-04-17 11:28:30 +0100

    Remove unused ProgressiveEnhancement component

--- wagtailio/static/js/main.js
+++ wagtailio/static/js/main.js
@@ -13,7 +13,6 @@ import DesktopSubMenu from './components/desktop-sub-menu';
 import DesktopCloseMenus from './components/desktop-close-menus';
 import CookieMessage from "./components/cookie-message";
 import SiteWideAlert from "./components/site-wide-alert";
-import ProgressiveEnhancement from "./components/progressive-enhancement";
 
 function initComponent(ComponentClass) {
     const items = document.querySelectorAll(ComponentClass.selector());
@@ -35,8 +34,6 @@ document.addEventListener('DOMContentLoaded', () => {
     initComponent(MobileMenu);
     initComponent(MobileSubMenu);
     initComponent(DesktopSubMenu);
-    initComponent(ProgressiveEnhancement);
-
     /* eslint-disable no-new */
     new DesktopCloseMenus();
 });
