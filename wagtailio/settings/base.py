708ecffe1b382641a46a7afefb1b62f72571f831
vossisboss <vossmk@gmail.com>
2024-10-23 17:33:17 -0400

    Bump django-storages.

--- wagtailio/settings/base.py
+++ wagtailio/settings/base.py
@@ -185,7 +185,7 @@ if env.get("BASIC_AUTH_ENABLED", "false").lower() == "true":
 # S3 configuration
 
 if "AWS_STORAGE_BUCKET_NAME" in env:
-    STORAGES["default"]["BACKEND"] = "storages.backends.s3boto3.S3Boto3Storage"
+    STORAGES["default"]["BACKEND"] = "storages.backends.s3.S3Storage"
     AWS_STORAGE_BUCKET_NAME = env["AWS_STORAGE_BUCKET_NAME"]
     AWS_S3_FILE_OVERWRITE = False
 
