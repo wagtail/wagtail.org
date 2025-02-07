12f7ee44a002d5fc1060056d16d5ee4fff7f58c5
Meagen Voss <45881480+vossisboss@users.noreply.github.com>
2025-02-07 17:49:00 -0500

    Forgot to run precommit.

--- wagtailio/core/migrations/0061_remove_excess_tables.py
+++ wagtailio/core/migrations/0061_remove_excess_tables.py
@@ -19,12 +19,7 @@ class Migration(migrations.Migration):
                 "DROP TABLE IF EXISTS wagtail_ab_testing_abtesthourlylog;",
                 "DROP TABLE IF EXISTS wagtail_ab_testing_abtest;",
                 "DROP TABLE IF EXISTS wagtail_image_import_driveidmapping;",
-                
             ],
-            reverse_sql=[
-                migrations.RunSQL.noop,
-                migrations.RunSQL.noop
-            ],
+            reverse_sql=[migrations.RunSQL.noop, migrations.RunSQL.noop],
         )
     ]
-
