5915650f30e813965aa1862012e48f3d2b16cbb5
Meagen Voss <45881480+vossisboss@users.noreply.github.com>
2025-07-07 23:01:20 -0400

    Additional sponsor styles.

--- wagtailio/wagtailspace/models.py
+++ wagtailio/wagtailspace/models.py
@@ -1,27 +1,39 @@
 from django.db import models
 
+from wagtail import blocks
 from wagtail.admin.panels import FieldPanel, MultiFieldPanel
 from wagtail.fields import RichTextField, StreamField
 from wagtail.models import Page
-from wagtail import blocks
-
-from wagtailio.core.blocks import CTABlock, SpaceStoryBlock, ImageBlock
-from wagtailio.utils.models import SocialMediaMixin
 from wagtail.snippets.models import register_snippet
+
+from wagtailio.core.blocks import CTABlock, ImageBlock, SpaceStoryBlock
+from wagtailio.utils.models import SocialMediaMixin
+
 
 @register_snippet
 class SpaceSocialSnippet(models.Model):
     heading = models.CharField(max_length=255)
     subheading = RichTextField(max_length=255)
-    social = StreamField([
-        ('social', blocks.StructBlock([
-            ('name', blocks.CharBlock()),
-            ('url', blocks.URLBlock()),
-            ('logo', ImageBlock(help_text="Max image width of 100px for best results.")),
-                ],
-            )),
-        ])
-    
+    social = StreamField(
+        [
+            (
+                "social",
+                blocks.StructBlock(
+                    [
+                        ("name", blocks.CharBlock()),
+                        ("url", blocks.URLBlock()),
+                        (
+                            "logo",
+                            ImageBlock(
+                                help_text="Max image width of 100px for best results."
+                            ),
+                        ),
+                    ],
+                ),
+            ),
+        ]
+    )
+
     class Meta:
         verbose_name = "Wagtail Space Social Menu"
 
@@ -49,12 +61,12 @@ class WagtailSpaceIndexPage(
     cta = StreamField([("cta", CTABlock())], blank=True, max_num=2)
     body = StreamField(SpaceStoryBlock(), blank=True)
     space_social = models.ForeignKey(
-          'wagtailspace.SpaceSocialSnippet',
-          null=True,
-          blank=True,
-          on_delete=models.SET_NULL,
-          related_name='+'
-      )
+        "wagtailspace.SpaceSocialSnippet",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="+",
+    )
 
     content_panels = Page.content_panels + [
         MultiFieldPanel(
@@ -68,7 +80,7 @@ class WagtailSpaceIndexPage(
             classname="collapsible",
         ),
         FieldPanel("body"),
-        FieldPanel("space_social")
+        FieldPanel("space_social"),
     ]
 
 
@@ -79,14 +91,14 @@ class WagtailSpacePage(SocialMediaMixin, Page):
     sub_heading = models.TextField(verbose_name="Sub heading", blank=True)
     body = StreamField(SpaceStoryBlock(), blank=True)
     space_social = models.ForeignKey(
-          'wagtailspace.SpaceSocialSnippet',
-          null=True,
-          blank=True,
-          on_delete=models.SET_NULL,
-          related_name='+'
-      )
+        "wagtailspace.SpaceSocialSnippet",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="+",
+    )
 
     content_panels = Page.content_panels + [
         FieldPanel("body"),
-        FieldPanel("space_social")
-        ]
+        FieldPanel("space_social"),
+    ]
